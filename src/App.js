267defd8d061aeef98ef3794081f93f5a7d7a6c9
krishanathep <krishanathep@gmail.com>
2020-02-20 14:52:44 +0700

    create monsters rolodex

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
-import './App.css';
 import { CardList } from './components/card-list/card-list.component'
+import { Header } from './components/layouts/Header'
+import { SearchBox } from './components/search-box/search.component'
+import './App.css';
 
 class App extends Component {
 constructor() {
   super()
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: '',
+      header: 'Monsters Rolodex'
     }
+
+    this.handleChange = this.handleChange.bind(this)  
   }
 
 componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        //.then(users => console.log(users)
         .then(users => this.setState({monsters: users}))
 }  
 
+handleChange = (e) => {
+  this.setState({ searchField: e.target.value })
+}
+
 render(){
+  const { monsters, searchField } = this.state
+  const filteredMonsters = monsters.filter(monster => 
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+
   return (
     <div className="App">
       <header className="App-header">
-        <CardList monsters={this.state.monsters} />           
+        <Header header={this.state.header} />
+        <SearchBox 
+          placeholder="search monster..."
+          handleChange={this.handleChange}
+        />  
+        <CardList monsters={filteredMonsters} />           
       </header>
     </div>
     );
