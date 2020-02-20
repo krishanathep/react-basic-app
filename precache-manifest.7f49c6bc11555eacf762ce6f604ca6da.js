82eb1031653029db8522a6dd5fda41d839cb3a4b
krishanathep <krishanathep@gmail.com>
2020-02-20 15:16:35 +0700

    Updates

---
+++ precache-manifest.7f49c6bc11555eacf762ce6f604ca6da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff1db6013cf049200e44a7cdcd62d2e",
+    "url": "/react-basic-app/index.html"
+  },
+  {
+    "revision": "d04f794404c3457da416",
+    "url": "/react-basic-app/static/css/main.2db530da.chunk.css"
+  },
+  {
+    "revision": "90149d1940b0dc97c2b7",
+    "url": "/react-basic-app/static/js/2.bb10f6b2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-basic-app/static/js/2.bb10f6b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d04f794404c3457da416",
+    "url": "/react-basic-app/static/js/main.fc87a49e.chunk.js"
+  },
+  {
+    "revision": "66497cb785aaca398378",
+    "url": "/react-basic-app/static/js/runtime-main.4fa014ea.js"
+  }
+]);
